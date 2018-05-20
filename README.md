7b65a833110ab5de60f6158dfc5d03a9b59c2f8f
Sunny Patel <sunnypatel165@gmail.com>
2018-05-20 23:32:27 +0530

    Added 804 Unique Morse Code Words

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Also most "famous" repos are C++ and Python based and I prefer Java these days o
 | # | Title | My Solution | LeetCode Solution | Difficulty |
 |---| ----- | ----------- | ------------------ | ---------- |
 |832|[Flipping an Image](https://leetcode.com/problems/flipping-an-image/description/)|[My Java Solution](https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/FlippingAnImage.java)|[LeetCode Solution](https://leetcode.com/articles/flipping-an-image/)|Easy
-|771|[Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description/)|[My Java Solution](https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/JewelsAndStones.java)|[LeetCode Solution](https://leetcode.com/articles/jewels-and-stones/)|Easy 
+|771|[Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description/)|[My Java Solution](https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/JewelsAndStones.java)|[LeetCode Solution](https://leetcode.com/articles/jewels-and-stones/)|Easy
+|804|[Unique Morse Code Words](https://leetcode.com/problems/unique-morse-code-words/description/)|[My Java Solution](https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/UniqueMorseCodeWords.java)|[LeetCode Solution](https://leetcode.com/articles/unique-morse-code-words/)|Easy 
---
+++ solutions/UniqueMorseCodeWords.java
@@ -0,0 +1,50 @@
+/*
+Problem Statement:
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+Return the number of different transformations among all words we have.
+
+Problem Link:
+    Unique Morse Code Words: https://leetcode.com/problems/unique-morse-code-words/description/
+Solution:
+    https://github.com/sunnypatel165/leetcode-again/solutions/UniqueMorseCodeWords.java
+
+Author:
+    Sunny Patel
+    sunnypatel165@gmail.com
+    https://github.com/sunnypatel165
+    https://www.linkedin.com/in/sunnypatel165/
+
+ */
+ class Solution {
+
+    private final String MORSE_MAPPING[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+    /**
+     * [convertToMorse Converts the given string into it Morse represetation]
+     * @param  s [String to be converted]
+     * @return   [Morse representation]
+     */
+    private String convertToMorse(String s){
+        StringBuilder builder = new StringBuilder();
+        for(int i =0;i<s.length();i++)
+            builder.append(MORSE_MAPPING[s.charAt(i)-'a']);
+        return builder.toString();
+    }
+
+    /**
+     * [uniqueMorseRepresentations returns the number of unique Morse Strings]
+     * @param  words [List of words to count the unique Morse on]
+     * @return       [Number of unique Morse words]
+     */
+    public int uniqueMorseRepresentations(String[] words) {
+
+        Set<String> set = new HashSet<>();
+        for(int i=0;i<words.length;i++)
+            set.add(convertToMorse(words[i]));
+        return set.size();
+
+    }
+}
