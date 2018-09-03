73f53018d8d7a0fd32a3b07e9b58ab2a59829ce2
sunnypatel165 <sunnydep@usc.edu>
2018-09-03 08:16:17 -0700

    Fixed syntax errors in NAry post order

--- solutions/NAryTreePostOrderTraversal.java
+++ solutions/NAryTreePostOrderTraversal.java
@@ -17,21 +17,6 @@ Author:
 
  */
 class Solution {
-    /*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val,List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-class Solution {
     public List<Integer> postorder(Node root) {
         List<Integer> list = new ArrayList<>();
         return postorderTravesal(root, list);
