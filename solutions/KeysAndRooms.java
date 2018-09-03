0c16a76ac8799191c982c482dee638633ed94782
sunnypatel165 <sunnydep@usc.edu>
2018-09-03 14:16:09 -0700

    Fixed README and Keys Rooms

--- solutions/KeysAndRooms.java
+++ solutions/KeysAndRooms.java
@@ -1,8 +1,10 @@
 /*
 Problem Statement:
-Given a binary tree, determine if it is height-balanced.
-For this problem, a height-balanced binary tree is defined as:
-    a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
+Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+Initially, all the rooms start locked (except for room 0).
+You can walk back and forth between rooms freely.
+Return true if and only if you can enter every room.
 
 Problem Link:
     Keys and Rooms: https://leetcode.com/problems/keys-and-rooms/description/
