2de95503eb1629002693bbe03f6e1475ebe0183d
sunnypatel165 <sunnydep@usc.edu>
2018-09-19 23:14:57 -0700

    Added 136 Single Number

---
+++ solutions/SingleNumber.java
@@ -0,0 +1,29 @@
+/*
+Problem Statement:
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Problem Link:
+    Assign Cookies: https://leetcode.com/problems/single-number/description/
+
+Solution:
+    https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/SingleNumber.java
+
+Author:
+    Sunny Patel
+    sunnypatel165@gmail.com
+    https://github.com/sunnypatel165
+    https://www.linkedin.com/in/sunnypatel165/
+
+ */
+ class Solution {
+     public int singleNumber(int[] nums) {
+         int ans = 0;
+         for(int i = 0;i<nums.length;i++ )
+             ans = ans ^ nums[i];
+         return ans;
+     }
+ }
