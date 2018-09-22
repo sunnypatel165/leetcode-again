44058f16c0a4bb5e3e01a2b1c49624d7ec990ffc
sunnypatel165 <sunnydep@usc.edu>
2018-09-21 23:21:02 -0700

    Added 342 power of four

---
+++ solutions/PowerOfFour.java
@@ -0,0 +1,28 @@
+/*
+Problem Statement:
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+
+Problem Link:
+    Assign Cookies: https://leetcode.com/problems/power-of-four/description/
+
+Solution:
+    https://github.com/sunnypatel165/leetcode-again/blob/master/solutions/PowerOfFour.java
+
+Author:
+    Sunny Patel
+    sunnypatel165@gmail.com
+    https://github.com/sunnypatel165
+    https://www.linkedin.com/in/sunnypatel165/
+
+ */
+ class Solution {
+    public boolean isPowerOfFour(int num) {
+        int count=1, copy =num;
+        while(num>0){
+            num>>=1;
+            count++;
+        }
+        return count%2==0 && (copy&copy-1)==0;
+    }
+}
