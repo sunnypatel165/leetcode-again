3eb17ee9b2994b20a62c7c8b34a3a30311839542
sunnypatel165 <sunnydep@usc.edu>
2019-08-26 11:20:34 +0530

    Fixes

--- solutions/MinimumDepthOfABinaryTree.java
+++ solutions/MinimumDepthOfABinaryTree.java
@@ -18,7 +18,47 @@ Author:
 
  */
 class Solution {
-    public int minDepth(TreeNode root){
+    public int minDepth(TreeNode root) {
+        return minDepthIterative(root);
+        // return minDepthRecursive(root);
+    }
+    public int minDepthIterative(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int min = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i =0;i<size;i++){
+                TreeNode t= queue.remove();
+                if(t.left==null && t.right==null){
+                    return min;
+                }
+                if(t.left!=null){
+                    queue.add(t.left);
+                }
+                if(t.right!=null){
+                    queue.add(t.right);
+                }
+            }
+            min++;
+        }
+        return 0;
+    }
+    public int minDepthRecursive(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left==0 || right ==0 ){
+            return left + right + 1;
+        }
+        return 1+ Math.min(left, right);
+    }
+    public int minDepthRecursiveOriginal(TreeNode root){
         if(root==null)
             return 0;
         if(root.left==null && root.right == null)
